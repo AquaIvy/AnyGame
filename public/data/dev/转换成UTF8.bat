56e6f2e9d593a19741d0f68712db2fa69cb96af3
AquaIvy <uMirrorcle@outlook.com>
2017-02-21 23:37:26 +0800

    minor

--- public/data/dev/转换成UTF8.bat
+++ public/data/dev/转换成UTF8.bat
@@ -1,6 +1,6 @@
 AnsiToUtf8.exe client *.csv
 AnsiToUtf8.exe server *.csv
 
-xcopy server\*.csv D:\Unity\Projects\AnyGame\program\platform\android\dev\AnyGame_vs\Server\TradeAge.Server.Game\bin\ConfigData /y /s /q
+xcopy server\*.csv ..\..\..\program\platform\android\dev\AnyGame_vs\Server\TradeAge.Server.Game\bin\ConfigData /y /s /q
 
 pause
